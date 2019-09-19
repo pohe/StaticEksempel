e77e53286dbc0b4f08c354598b0ddb1a38c32ae8
Poul Henriksen <pohe@easj.dk>
2019-09-19 14:08:40 +0200

    Add project files.

---
+++ Sandbox/Calculator.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Sandbox
+{
+    public static class Calculator
+    {
+
+        // Add two int numbers return an int
+        public static int Add(int a, int b)
+        {
+            return a + b;
+        }
+
+        //Minus beregning på to int
+
+        public static int Minus(int a, int b)
+        {
+            return a - b;
+        }
+
+        //Multiplikation af to int tal
+
+        public static int Mult(int jens, int oskar)
+        {
+            return jens * oskar;
+        }
+
+        //DivisionInt af to heltal retur heltal
+
+        public static int DivisionInt(int a, int b)
+        {
+            return a / b;
+        }
+
+
+        //DivisionDec af to heltal retur decimal
+
+        public static double DivisionDec(int a, int b)
+        {
+            return a / (b*1.0);
+        }
+
+
+
+    }
+}
---
+++ Sandbox/InsertCodeHere.cs
@@ -0,0 +1,20 @@
+﻿using System;
+
+namespace Sandbox
+{
+    public class InsertCodeHere
+    {
+        public void MyCode()
+        {
+            // The FIRST line of code should be BELOW this line
+
+            //Calculator c = new Calculator();
+            int addResult= Calculator.Add(12, 23);
+            double divDecResult = Calculator.DivisionDec(23, 12);
+            
+            Console.WriteLine(divDecResult);
+
+            // The LAST line of code should be ABOVE this line
+        }
+    }
+}
